642fb2d1fc055dd0cba80b61bdde43fbb1d816a5
ozan isgor <ozanisgor11@gmail.com>
2024-10-28 18:27:35 +0300

    feat: query single project

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,3 +1,31 @@
+import { useQuery } from "@apollo/client";
+import { Link, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { GET_PROJECT } from "../queries/projectQueries";
+
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, {
+    variables: { id },
+  });
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <>
+      {!loading && !error && (
+        <div className="mx-auto w-75 card p-5">
+          <Link className="btn btn-light btn-sm w-25 d-inline ms-auto" to={`/`}>
+            Back
+          </Link>
+          <h2>{data.project.name}</h2>
+          <p>{data.project.description}</p>
+
+          <h5 className="mt-3">Project Status</h5>
+          <p className="lead">{data.project.status}</p>
+        </div>
+      )}
+    </>
+  );
 }
