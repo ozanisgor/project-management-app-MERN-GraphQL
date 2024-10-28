487d97efa04b8bb115585e3b1bd9153245bf4259
ozan isgor <ozanisgor11@gmail.com>
2024-10-28 17:49:32 +0300

    feat: fetch & display projects

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import AddClientModal from "./components/AddClientModal";
 import Clients from "./components/Clients";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
@@ -15,6 +16,7 @@ function App() {
         <Header />
         <div className="container">
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
