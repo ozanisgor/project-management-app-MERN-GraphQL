71dd0fe8bcc666ea983b43eca73df55add7a8822
ozan isgor <ozanisgor11@gmail.com>
2024-10-29 10:26:08 +0300

    feat: client info and add project modal components

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { Link, useParams } from "react-router-dom";
+import ClientInfo from "../components/ClientInfo";
 import Spinner from "../components/Spinner";
 import { GET_PROJECT } from "../queries/projectQueries";
 
@@ -24,6 +25,8 @@ export default function Project() {
 
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
+
+          <ClientInfo client={data.project.client} />
         </div>
       )}
     </>
